178a653aaf2e243f5a99a5297870e81735bd8cd4
rodrigo <rodrigomachacajimenez312@gmail.com>
2019-09-27 09:47:06 -0400

    mi primer commit

---
+++ Pizza2/Pizza2/carnivora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza2
+{
+    public class carnivora:pizza
+    {
+    }
+    public class Carne
+    {
+
+    }
+    public class Salchicha
+    {
+
+    }
+}
---
+++ Pizza2/Pizza2/pizza.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza2
+{
+    public class pizza
+    {
+        public Refresco refresco { get; set; }
+        public string Tamaño { get; set; }
+        public string Tipo { get; set; }
+        public int Porciones { get; set; }
+
+        public pizza()
+        {
+            
+            this.queso = new queso();
+            this.oregano = new oregano();
+        }
+        public queso queso { get; set; }
+        public oregano oregano { get; set; }
+    }
+
+    
+    public class queso
+    {
+
+    }
+    public class oregano
+    {
+
+    }
+    public class Refresco
+    {
+        public string nombre { get; set; }
+        public int litros { get; set; }
+    }
+}
